2daa8652b355889fd8177ed3beedf0afceec6db8
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-01-17 23:07:03 +0100

    first commit

--- src/main/java/com/example/eagle/data/repositories/LibraryRepo.java
+++ src/main/java/com/example/eagle/data/repositories/LibraryRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+
 //@Repository
 public interface LibraryRepo extends JpaRepository<Library, Long> {
 }
